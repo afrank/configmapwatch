ddfffcaa1e6a0c49ceca7e34cfa4a947804d7518
Adam Frank <afrank@mozilla.com>
2020-02-27 18:03:14 -0800

    refactored to make simpler

--- watch.py
+++ watch.py
@@ -11,31 +11,27 @@ class Config:
         self.cfg_vol = cfg_vol
         self.cfg_file = cfg_file
         self._load_config()
-        self._handler = self.ReadConfig(self)
+        self._handler = FileSystemEventHandler()
+        # I use on_deleted because the last thing that happens when a config is updated is the old one is deleted
+        self._handler.on_deleted = self._event_handler
         self._observer = Observer()
         self._observer.schedule(self._handler,self.cfg_vol,recursive=True)
         self._observer.start()
+    def _event_handler(self,event):
+        if "config.json" in event.src_path:
+            logging.info(f"detected a config change; re-reading config.json.")
+            self._load_config()
     def _load_config(self):
         self._config = json.load(open(f"{self.cfg_vol}/{self.cfg_file}"))
     def __iter__(self):
         return iter(self._config.items())
-    @property
-    def config(self):
-        return _config
-    class ReadConfig(FileSystemEventHandler):
-        def __init__(self,outer):
-            super(FileSystemEventHandler).__init__()
-            self.outer = outer
-        def dispatch(self, event):
-            if "config.json" in event.src_path and event.event_type == 'deleted':
-                logging.info(f"detected a config change; re-reading config.json.")
-                self.outer._load_config()
+    def __del__(self):
+        self._observer.stop()
+        self._observer.join()
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO,format='%(asctime)s - %(message)s',datefmt='%Y-%m-%d %H:%M:%S')
     cfg = Config()
     while True:
-        logging.info("Here is my config:")
         logging.info(dict(cfg))
         time.sleep(3)
-    observer.join()
